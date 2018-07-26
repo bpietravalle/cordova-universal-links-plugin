927a3ac28e04e30c4cbbde2765c0e7c689251e1b
Brian Pietravalle <bpietravalle@gmail.com>
2018-07-25 21:45:22 -0400

    fix hook for cordova 7

--- hooks/lib/ios/xcodePreferences.js
+++ hooks/lib/ios/xcodePreferences.js
@@ -14,7 +14,7 @@ var COMMENT_KEY = /_comment$/;
 var context;
 
 module.exports = {
-  enableAssociativeDomainsCapability: enableAssociativeDomainsCapability
+    enableAssociativeDomainsCapability: enableAssociativeDomainsCapability
 }
 
 // region Public API
@@ -25,18 +25,18 @@ module.exports = {
  * @param {Object} cordovaContext - cordova context object
  */
 function enableAssociativeDomainsCapability(cordovaContext) {
-  context = cordovaContext;
+    context = cordovaContext;
 
-  var projectFile = loadProjectFile();
+    var projectFile = loadProjectFile();
 
-  // adjust preferences
-  activateAssociativeDomains(projectFile.xcode);
+    // adjust preferences
+    activateAssociativeDomains(projectFile.xcode);
 
-  // add entitlements file to pbxfilereference
-  addPbxReference(projectFile.xcode);
+    // add entitlements file to pbxfilereference
+    addPbxReference(projectFile.xcode);
 
-  // save changes
-  projectFile.write();
+    // save changes
+    projectFile.write();
 }
 
 // endregion
@@ -51,33 +51,33 @@ function enableAssociativeDomainsCapability(cordovaContext) {
  * @param {Object} xcodeProject - xcode project preferences; all changes are made in that instance
  */
 function activateAssociativeDomains(xcodeProject) {
-  var configurations = nonComments(xcodeProject.pbxXCBuildConfigurationSection());
-  var entitlementsFilePath = pathToEntitlementsFile();
-  var config;
-  var buildSettings;
-  var deploymentTargetIsUpdated;
+    var configurations = nonComments(xcodeProject.pbxXCBuildConfigurationSection());
+    var entitlementsFilePath = pathToEntitlementsFile();
+    var config;
+    var buildSettings;
+    var deploymentTargetIsUpdated;
 
-  for (config in configurations) {
-    buildSettings = configurations[config].buildSettings;
-    buildSettings['CODE_SIGN_ENTITLEMENTS'] = '"' + entitlementsFilePath + '"';
+    for (config in configurations) {
+        buildSettings = configurations[config].buildSettings;
+        buildSettings['CODE_SIGN_ENTITLEMENTS'] = '"' + entitlementsFilePath + '"';
 
-    // if deployment target is less then the required one - increase it
-    if (buildSettings['IPHONEOS_DEPLOYMENT_TARGET']) {
-      if (compare(buildSettings['IPHONEOS_DEPLOYMENT_TARGET'], IOS_DEPLOYMENT_TARGET) == -1) {
-        buildSettings['IPHONEOS_DEPLOYMENT_TARGET'] = IOS_DEPLOYMENT_TARGET;
-        deploymentTargetIsUpdated = true;
-      }
-    } else {
-      buildSettings['IPHONEOS_DEPLOYMENT_TARGET'] = IOS_DEPLOYMENT_TARGET;
-      deploymentTargetIsUpdated = true;
+        // if deployment target is less then the required one - increase it
+        if (buildSettings['IPHONEOS_DEPLOYMENT_TARGET']) {
+            if (compare(buildSettings['IPHONEOS_DEPLOYMENT_TARGET'], IOS_DEPLOYMENT_TARGET) == -1) {
+                buildSettings['IPHONEOS_DEPLOYMENT_TARGET'] = IOS_DEPLOYMENT_TARGET;
+                deploymentTargetIsUpdated = true;
+            }
+        } else {
+            buildSettings['IPHONEOS_DEPLOYMENT_TARGET'] = IOS_DEPLOYMENT_TARGET;
+            deploymentTargetIsUpdated = true;
+        }
     }
-  }
 
-  if (deploymentTargetIsUpdated) {
-    console.log('IOS project now has deployment target set as: ' + IOS_DEPLOYMENT_TARGET);
-  }
+    if (deploymentTargetIsUpdated) {
+        console.log('IOS project now has deployment target set as: ' + IOS_DEPLOYMENT_TARGET);
+    }
 
-  console.log('IOS project Code Sign Entitlements now set to: ' + entitlementsFilePath);
+    console.log('IOS project Code Sign Entitlements now set to: ' + entitlementsFilePath);
 }
 
 // endregion
@@ -90,16 +90,16 @@ function activateAssociativeDomains(xcodeProject) {
  * @param {Object} xcodeProject - xcode project preferences; all changes are made in that instance
  */
 function addPbxReference(xcodeProject) {
-  var fileReferenceSection = nonComments(xcodeProject.pbxFileReferenceSection());
-  var entitlementsFileName = path.basename(pathToEntitlementsFile());
+    var fileReferenceSection = nonComments(xcodeProject.pbxFileReferenceSection());
+    var entitlementsFileName = path.basename(pathToEntitlementsFile());
 
-  if (isPbxReferenceAlreadySet(fileReferenceSection, entitlementsFileName)) {
-    console.log('Entitlements file is in reference section.');
-    return;
-  }
+    if (isPbxReferenceAlreadySet(fileReferenceSection, entitlementsFileName)) {
+        console.log('Entitlements file is in reference section.');
+        return;
+    }
 
-  console.log('Entitlements file is not in references section, adding it');
-  xcodeProject.addResourceFile(entitlementsFileName);
+    console.log('Entitlements file is not in references section, adding it');
+    xcodeProject.addResourceFile(entitlementsFileName);
 }
 
 /**
@@ -110,19 +110,19 @@ function addPbxReference(xcodeProject) {
  * @return true - if reference is set; otherwise - false
  */
 function isPbxReferenceAlreadySet(fileReferenceSection, entitlementsRelativeFilePath) {
-  var isAlreadyInReferencesSection = false;
-  var uuid;
-  var fileRefEntry;
+    var isAlreadyInReferencesSection = false;
+    var uuid;
+    var fileRefEntry;
 
-  for (uuid in fileReferenceSection) {
-    fileRefEntry = fileReferenceSection[uuid];
-    if (fileRefEntry.path && fileRefEntry.path.indexOf(entitlementsRelativeFilePath) > -1) {
-      isAlreadyInReferencesSection = true;
-      break;
+    for (uuid in fileReferenceSection) {
+        fileRefEntry = fileReferenceSection[uuid];
+        if (fileRefEntry.path && fileRefEntry.path.indexOf(entitlementsRelativeFilePath) > -1) {
+            isAlreadyInReferencesSection = true;
+            break;
+        }
     }
-  }
 
-  return isAlreadyInReferencesSection;
+    return isAlreadyInReferencesSection;
 }
 
 // region Xcode project file helpers
@@ -133,29 +133,22 @@ function isPbxReferenceAlreadySet(fileReferenceSection, entitlementsRelativeFile
  * @return {Object} projectFile - project file information
  */
 function loadProjectFile() {
-   var platform_ios;
-   var projectFile;
-
-   try {
-     // try pre-5.0 cordova structure
-     platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms')['ios'];
-     projectFile = platform_ios.parseProjectFile(iosPlatformPath());
-   } catch (e) {
-    // let's try cordova 5.0 structure
-    platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms/ios');
-    projectFile = platform_ios.parseProjectFile(iosPlatformPath());
-   try {
-      // let's try cordova 5.0 structure
-      platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms/ios');
-      projectFile = platform_ios.parse(iosPlatformPath());
+    var platform_ios;
+    var projectFile;
+    // we need to update for cordova 8.0
+    try {
+        // let's try cordova 5.0 structure
+        platform_ios = context.requireCordovaModule('cordova-lib/src/plugman/platforms/ios');
+        projectFile = platform_ios.parse(iosPlatformPath());
     } catch (e) {
-      // try cordova 7.0 structure
-      var iosPlatformApi = require(path.join(iosPlatformPath(), '/cordova/Api'));
-      var projectFileApi = require(path.join(iosPlatformPath(), '/cordova/lib/projectFile.js'));
-      var locations = (new iosPlatformApi()).locations;
-      projectFile = projectFileApi.parse(locations);
+        // try cordova 7.0 structure
+        var iosPlatformApi = require(path.join(iosPlatformPath(), '/cordova/Api'));
+        var projectFileApi = require(path.join(iosPlatformPath(), '/cordova/lib/projectFile.js'));
+        var locations = (new iosPlatformApi()).locations;
+        projectFile = projectFileApi.parse(locations);
     }
-   }
+    return projectFile;
+}
 
 /**
  * Remove comments from the file.
@@ -164,16 +157,16 @@ function loadProjectFile() {
  * @return {Object} file object without comments
  */
 function nonComments(obj) {
-  var keys = Object.keys(obj);
-  var newObj = {};
+    var keys = Object.keys(obj);
+    var newObj = {};
 
-  for (var i = 0, len = keys.length; i < len; i++) {
-    if (!COMMENT_KEY.test(keys[i])) {
-      newObj[keys[i]] = obj[keys[i]];
+    for (var i = 0, len = keys.length; i < len; i++) {
+        if (!COMMENT_KEY.test(keys[i])) {
+            newObj[keys[i]] = obj[keys[i]];
+        }
     }
-  }
 
-  return newObj;
+    return newObj;
 }
 
 // endregion
@@ -181,19 +174,19 @@ function nonComments(obj) {
 // region Path helpers
 
 function iosPlatformPath() {
-  return path.join(projectRoot(), 'platforms', 'ios');
+    return path.join(projectRoot(), 'platforms', 'ios');
 }
 
 function projectRoot() {
-  return context.opts.projectRoot;
+    return context.opts.projectRoot;
 }
 
 function pathToEntitlementsFile() {
-  var configXmlHelper = new ConfigXmlHelper(context),
-    projectName = configXmlHelper.getProjectName(),
-    fileName = projectName + '.entitlements';
+    var configXmlHelper = new ConfigXmlHelper(context),
+        projectName = configXmlHelper.getProjectName(),
+        fileName = projectName + '.entitlements';
 
-  return path.join(projectName, 'Resources', fileName);
+    return path.join(projectName, 'Resources', fileName);
 }
 
 // endregion
